45491cfd41872478448f4b78f6f145b8dc3e9ad0
GeneralGustavo <kingval1020@gmail.com>
2023-03-27 04:51:04 -0700

    built navbar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 
+
 import './App.css';
 import Home from './components/Home';
 import { Routes, Route } from "react-router-dom";
--- client/src/components/Partials/Header/Header.js
+++ client/src/components/Partials/Header/Header.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import "./Header.css"
+import {Link} from 'react-router-dom';
+import "./Header.css";
+// import headStyle from './Header.Modules.css';
+import logo from "../../../asset/CMA-v2.svg";
+import {FaBars, FaUserShield, FaUserPlus} from 'react-icons/fa';
+
 
 const Header = () => {
   return (
-    <div>
-        <h1>Header</h1>
-    </div>
+    <nav className="main-navbar">
+        <div className='logo-box'>
+          <Link to="/">
+            <img src={logo} alt='CMA Logo'/>
+          </Link>
+        </div>
+        <div className='nav-link-grp'>
+          <div className='nav-toggler'>
+            <span id='toggoleBtn'>
+            <FaBars/>
+            </span>
+          </div>
+          <div className='link-grp'>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/services">Services</Link>
+            <Link to="/contact">Contact</Link>
+            <Link className='nav-login-btn' to="/login">
+              Login
+              <FaUserShield/>
+            </Link>
+            <Link className='nav-signup-btn' to="/signup">
+              Signup
+              <FaUserPlus/>
+            </Link>
+          </div>
+        </div>
+    </nav>
   )
 }
 
