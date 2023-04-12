c8e230bc0172436773a992105d6e3cbcd48755e5
GeneralGustavo <kingval1020@gmail.com>
2023-04-11 21:26:37 -0700

    created usersController and userModel

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,3 +1,4 @@
+const userModel = require('../models/userModel');
 const UserModel = require('../models/userModel');
 const bcrypt = require("bcrypt");
 
@@ -12,38 +13,84 @@ const registerUser = async(req, res) => {
         const foundUser = await UserModel.find({email:email});
 
         // if user found, 
-        if (foundUser) {
-            res.status(400).jason({message: "User already exsist"});
-            returen;
+        if (!foundUser) {
+            res.status(400).json({message: "User already exsist"});
+            return;
         }
 
         // if not found, create a new user
+
+        // create a salt
+        const salt = 10;
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(password, salt);
+        // console.log(hashedPassword);
+
+        // create new user
         const newUser = await UserModel.create({
             first_name: first_name,
             last_name: last_name,
             email: email,
             phone: phone,
-            password: '',
+            password: hashedPassword,
         });
 
 
-        
+     // if user was not created successfully
+     if (!newUser) {
+        res.status(400);
+        throw new Error("Unable to create User");
+     } 
+
+     // response to front-end
+     res.status(201).json({
+        message: "User created!",
+        data: newUser,
+    });
+
     } catch (error) {
         res.status(500)
         throw new Error(`Internal server error:${error}`);
     }
 
-    res.json({
-        message: "User created!",
-        data: req.body,
-    });
+    // res.json({
+    //     message: "User created!",
+    //     data: req.body,
+    // });
 };
 
 // @define:login user
 // @route: /api/users/login
 // @privacy: public
 const loginUser = async(req, res) => {
-    res.json({message: "User loggedIn!"});
+    // destructure email and password from req body
+    const { email, password } = req.body;
+
+    try{
+        // check if email exist
+        const foundEmail = await userModel.find({email:email });
+        // console.log(foundEmail);
+
+        const checkPassword = await bcrypt.compare( password, foundEmail.password );
+        console.log(checkPassword);
+
+        //check if password match
+        // if( foundEmail && (await bcrypt.compare(password, foundEmail.password))) {
+        //     res.status(400).json({ message:"Invalid credentials"});
+        // }
+
+        // if loggin details are correct
+        res.json({
+            message: "User loggedIn",
+            data: foundEmail
+        });
+
+        // res.json({message: "User loggedIn!"});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+    
 }
 
 // @define: get single user
@@ -72,7 +119,7 @@ const updateSingleUser = async(req, res) => {
 // @privacy: proected
 const deleteSingleUser = async(req, res) => {
     res.json({message: "delete single user!"});
-}
+};
 
 module.exports = { 
     registerUser, 
